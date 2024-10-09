3dabf64a108e6974b259b14fc0b2ce74f713b3b4
RiverNguyen <ngiang1309@gmail.com>
2024-10-09 18:28:44 +0700

    CREATE: modules

--- src/modules/users/users.module.ts
+++ src/modules/users/users.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
-import { UsersService } from './users.service';
-import { UsersController } from './users.controller';
 import { MongooseModule } from '@nestjs/mongoose';
-import { User, UserSchema } from './schemas/user.schema';
+
+import { UsersService } from '@/modules/users/users.service';
+import { UsersController } from '@/modules/users/users.controller';
+import { User, UserSchema } from '@/modules/users/schemas/user.schema';
 
 @Module({
   imports: [
